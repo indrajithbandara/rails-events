202a0b20d9df57f1e644e2fe5090213b0f5518c4
david-dacruz <dvddcrz@gmail.com>
2018-02-07 20:27:46 +0100

    allez

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   # '/' route in pages_controller def home
-  root 'pages#home', as: 'homepage'
+  root 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
